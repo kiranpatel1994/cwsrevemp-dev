1f53c7264b4e1c0b01bc0758a7013a6184f7cc44
henry-cws <henry@cwsio.com>
2023-07-19 19:55:07 +0530

    Portfolio-detail_full code

--- pages/portfolio/portfolio-detail.js
+++ pages/portfolio/portfolio-detail.js
@@ -1,9 +1,9 @@
 function PortfolioDetail() {
   return (
-  <main>
+  <main className="position-relative zindex-2">
     <section className="in_project">
-      <div className="container-xl">
-        <div className="row">
+      <div className="container-xl p-0">
+        <div className="row g-0">
           <div className="col-12 col-md-10">
             <h1>YM Ventures </h1>
             <div className="list-out-tag">
@@ -14,14 +14,128 @@ function PortfolioDetail() {
                 <li className="list-inline-item"><a href="#">Industry tag </a> </li>
               </ul>
             </div>
-            <p>Our collaboration with YM Ventures involved the comprehensive design and development of their dynamic and engaging website. With a keen focus on showcasing their innovative solutions and fostering a seamless user experience, our team crafted a visually captivating interface that perfectly aligned with YM Ventures' <strong>brand identity. </strong> </p>
+            <p>Our collaboration with YM Ventures involved the comprehensive design and development of their dynamic and engaging website. With a keen focus on showcasing their innovative solutions and fostering a seamless user experience, our team crafted a visually captivating interface that perfectly aligned with <strong>YM Ventures' brand identity. </strong> </p>
           </div>
-          <div className="col-12 col-md-2">
-            
+          <div className="col-12 col-md-2 align-self-end">
+            <a href="#" className="site_link" target="_blank">
+              <span>Go to website </span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="17.548" height="17.548" viewBox="0 0 17.548 17.548">
+                <g id="Group_5436" data-name="Group 5436" transform="translate(-6 -6)">
+                  <path id="Path_10679" data-name="Path 10679" d="M22.086,23.548a1.328,1.328,0,0,1-1.024-.439L6.439,8.486A1.448,1.448,0,0,1,8.486,6.439L23.109,21.062a1.414,1.414,0,0,1,0,2.047,1.328,1.328,0,0,1-1.024.439Z" fill="#fdb627"/>
+                  <path id="Path_10680" data-name="Path 10680" d="M20.829,22.212H8.383a1.383,1.383,0,1,1,0-2.766H19.446V8.383a1.383,1.383,0,1,1,2.766,0V20.829A1.307,1.307,0,0,1,20.829,22.212Z" transform="translate(1.336 1.336)" fill="#fdb627"/>
+                </g>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
     </section>
+    <section className="gll__inner">
+      <div className="container-fluid">
+        <div className="row gll__inner-col">
+          <div className="col-12">
+            <img className="w-100 img-fluid" src="../images/ventures-1.png" />
+          </div>
+        </div>
+        <div className="row gll__inner-col g-2">
+          <div className="col-12 col-md-6">
+            <img className="w-100 img-fluid" src="../images/ventures-2.png" />
+          </div>
+          <div className="col-12 col-md-6">
+            <img className="w-100 img-fluid" src="../images/ventures-9.png" />
+          </div>
+        </div>
+        <div className="row gll__inner-col">
+          <div className="col-12">
+            <img className="w-100 img-fluid" src="../images/ventures-3.png" />
+          </div>
+        </div>
+        <div className="row gll__inner-col">
+          <div className="col-12">
+            <img className="w-100 img-fluid" src="../images/ventures-5.png" />
+          </div>
+        </div>
+        <div className="row gll__inner-col">
+          <div className="col-12">
+            <img className="w-100 img-fluid" src="../images/ventures-7.png" />
+          </div>
+        </div>
+        <div className="row gll__inner-col">
+          <div className="col-12">
+            <img className="w-100 img-fluid" src="../images/ventures-6.png" />
+          </div>
+        </div>
+        <div className="row gll__inner-col">
+          <div className="col-12">
+            <img className="w-100 img-fluid" src="../images/ventures-4.png" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <div className="more_content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 text-center">
+            <h2>More like <span>this</span>. </h2>
+          </div>
+        </div>
+        <div className="project-contaner overflow-hidden">
+          <div className="row g-15">
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="gif_placer">
+                <div className="gif_box">
+                  <img className="img-fluid w-100" src="../images/placeholder.png" />
+                </div>
+                <div className="gradient_wall">
+                  <a href="#" className="explore_btn">
+                    <span>Explore <img src="../images/down-right.png" /></span>
+                  </a>
+                  <div className="gradient_box">
+                    <h3>YM Ventures </h3>
+                    <span className="fake_button">Website Design & Development </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="gif_placer">
+                <div className="gif_box">
+                  <img className="img-fluid w-100" src="../images/placeholder.png" />
+                </div>
+                <div className="gradient_wall">
+                  <a href="#" className="explore_btn">
+                    <span>Explore <img src="../images/down-right.png" /></span>
+                  </a>
+                  <div className="gradient_box">
+                    <h3>YM Ventures </h3>
+                    <span className="fake_button">Website Design & Development </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="gif_placer">
+                <div className="gif_box">
+                  <img className="img-fluid w-100" src="../images/placeholder.png" />
+                </div>
+                <div className="gradient_wall">
+                  <a href="#" className="explore_btn">
+                    <span>Explore <img src="../images/down-right.png" /></span>
+                  </a>
+                  <div className="gradient_box">
+                    <h3>YM Ventures </h3>
+                    <span className="fake_button">Website Design & Development </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="text-center lets_talk_post">
+          <a href="#" className="lets_talk"> <em>Let's talk </em> <span> <svg xmlns="http://www.w3.org/2000/svg" width="35.798" height="35.799" viewBox="0 0 35.798 35.799"><g id="down-right" transform="translate(-8.485 17.899) rotate(-45)"><path id="Path_10679" data-name="Path 10679" d="M29.2,31.313a1.915,1.915,0,0,1-1.477-.633L6.633,9.586A2.088,2.088,0,1,1,9.586,6.633L30.68,27.727a2.039,2.039,0,0,1,0,2.953,1.915,1.915,0,0,1-1.477.633Z" transform="translate(0)" fill="#07042b"/><path id="Path_10680" data-name="Path 10680" d="M26.948,28.943H8.995a1.995,1.995,0,0,1,0-3.99H24.953V8.995a1.995,1.995,0,0,1,3.99,0V26.948A1.885,1.885,0,0,1,26.948,28.943Z" transform="translate(2.37 2.37)" fill="#07042b"/> </g></svg> </span> </a>
+        </div>
+      </div>
+    </div>
   </main>
   );
 }
