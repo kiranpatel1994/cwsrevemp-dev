8d72d38311c635f076327d8ffb7a6adf025665e5
kiranmanektech <kiran.p@manektech.com>
2023-09-29 11:30:33 +0530

    Design Changes

--- components/propertyManagement/propertyManagement.js
+++ components/propertyManagement/propertyManagement.js
@@ -527,10 +527,12 @@ export default function PropertyManagement({ data, form, themeOptions }) {
                     className="col-12 col-md-6 col-lg-4"
                     key={`property-${index}`}
                   >
-                    <img
-                      className="img-fluid"
-                      src={item.screenImage.sourceUrl}
-                    />
+                    <a href={item.screenWebsiteLink}>
+                      <img
+                        className="img-fluid"
+                        src={item.screenImage.sourceUrl}
+                      />
+                    </a>
                   </div>
                 );
               })}
