475bcca6536a77c95030d445d92ef0b5b567e8d0
henry-cws <henry@cwsio.com>
2023-12-05 15:13:35 +0530

    Website-maintenance-plans
    Update and Responsive

--- pages/website-maintenance-plans.js
+++ pages/website-maintenance-plans.js
@@ -2,12 +2,20 @@ import GraphAPI from "../services/graphQL";
 const WebsiteMaintenance = () => {
   return (
     <>
-      <h1>Website Maintenance Plans</h1>
+    <main className="wesite_maintenance">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="mb-3">Website Maintenance Plans</h1>
+          </div>
+        </div>
+      </div>
       <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
       <stripe-pricing-table
         pricing-table-id="prctbl_1OHxJWJWYUGeMyAR9deBXLLT"
         publishable-key="pk_live_cnlOmSdLR4yX7Wp8N2RKj4yk"
       ></stripe-pricing-table>
+    </main>
     </>
   );
 };
